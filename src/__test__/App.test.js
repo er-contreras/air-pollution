2e1a93b47f936e8cb5fd2306fe00dae3dfcbfce5
Erick Contreras <eri89--@hotmail.com>
2021-09-20 21:20:41 -0500

    Implement tests

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,16 +1,40 @@
-import React from 'react'; // eslint-disable-line
-import { render, screen } from '@testing-library/react'; // eslint-disable-line
+import React from 'react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-// import Forecast from '../components/Forecast';
 import App from '../App';
 
-describe('Describe Section', () => {
+describe('Testing Rendering "App" component', () => {
 
-  it('Testing 1', () => {
+  it('Look for String "Current Air Pollution in the component"', () => {
     const component = render(<App />);
-    console.log(component.debug());
-    // const wrapper = component.findByRole('#input');
-    // expect(wrapper.length).toBe(1);
+    // console.log(component.debug());
+    const whatever = screen.getByText(/Current Air Pollution/i);
+    expect(whatever).toBeInTheDocument();
   });
+
+  it('Look for String "AIR POLLUTION INDEX BY COUNTRY"', () => {
+    render(<App />);
+    const whatever = screen.getByText(/AIR POLLUTION INDEX BY COUNTRY/i);
+    expect(whatever).toBeInTheDocument();
+  });
+
+  it('Not Look for String "AIR POLLUTION INDEX BY NAME"', () => {
+    render(<App />);
+    const whatever = screen.queryByText(/AIR POLLUTION INDEX BY NAME/i);
+    expect(whatever).not.toBeInTheDocument();
+  });
+
+  it('Not Look for String "AIR POLLUTION INDEX BY NAME"', async () => {
+    render(<App />);
+    const whatever = screen.getAllByRole("search");
+    expect(whatever.length).toBe(2);
+  });
+
+  // it('Look ASYNC Country name', async () => {
+  //   render(<App />);
+  //   // console.log(component.debug());
+  //   const whatever3 = await screen.findAllByText(/Mexico/i);
+  //   expect(whatever3).toBeInTheDocument();
+  // });
 
 })
